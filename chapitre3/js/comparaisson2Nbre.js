8614564b17152eb287419868c818a90062a40c56
Philippe Basuyau <geronimo8080@gmail.com>
2017-03-08 11:09:40 +0100

    Création dun logiciel qui compare 2 nombres pour savoir si ils sont égaux ou pas

---
+++ chapitre3/js/comparaisson2Nbre.js
@@ -0,0 +1,12 @@
+/*
+Comparaisson de 2 nombres
+*/
+
+var nbr1 = prompt("Entrez un nombre : ");
+var nbr2 = prompt ("Entrez un nombre : ");
+
+if (nbr1 === nbr2){
+    console.log("Les deux nombres sont égaux");
+}else{
+    console.log ("Les deux nombres sont différents")
+}
--- chapitre3/js/jourSuivant.js
+++ chapitre3/js/jourSuivant.js
@@ -26,7 +26,59 @@ if ((jour === "dimanche") || (jour === "Dimanche")){
 } else if ((jour === "samedi") || (jour === "Samedi")){
      console.log("Demain c'est Dimanche");
 } else {
-    console.log("Je n'ai pas très bien compris le jour !");
+    console.log("Je n'ai pas très bien compris le jour que nous sommes !");
+}
+
+
+/*
+Cas : Switch
+*/
+var jour = prompt("Quel jour sommes nous ? : ");
+switch (jour){
+    case "dimanche":
+        console.log("Demain c'est Lundi");
+        break;
+     case "Dimanche":
+        console.log("Demain c'est Lundi");
+        break;
+     case "lundi":
+        console.log("Demain c'est Mardi");
+        break;
+     case "Lundi":
+        console.log("Demain c'est Mardi");
+        break;
+     case "mardi":
+        console.log("Demain c'est Lundi");
+        break;
+     case "Mardi":
+        console.log("Demain c'est Mercredi");
+        break;
+      case "mercredi":
+        console.log("Demain c'est Jeudi");
+        break;
+     case "Mercredi":
+        console.log("Demain c'est Jeudi");
+        break;
+     case "jeudi":
+        console.log("Demain c'est Vendredi");
+        break;
+     case "Jeudi":
+        console.log("Demain c'est Vendredi");
+        break;
+     case "vendredi":
+        console.log("Demain c'est Samedi");
+        break;
+     case "Vendredi":
+        console.log("Demain c'est Samedi");
+        break;
+     case "samedi":
+        console.log("Demain c'est Dimanche");
+        break;
+     case "Samedi":
+        console.log("Demain c'est Dimanche");
+        break;
+default:
+    console.log("Je n'ai pas très bien compris le jour que nous sommes !");
 }
 
 
