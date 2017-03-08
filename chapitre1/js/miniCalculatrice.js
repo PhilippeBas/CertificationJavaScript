5edc89362eff402300b14d87b7d9877624056d51
Philippe Basuyau <geronimo8080@gmail.com>
2017-03-08 08:05:52 +0100

    Creation d'une mini calculette, donc miniCalcutrice.html et miniCalcutrice.js

---
+++ chapitre1/js/miniCalculatrice.js
@@ -0,0 +1,9 @@
+console.log("Avec les chiffre 6 & 3");
+console.log("\nRésultat pour l'addition: ");
+console.log(6+3);
+console.log("\nRésultat pour la soustraction: ");
+console.log(6-3);
+console.log("\nRésultat pour la multiplication: ");
+console.log(6*3);
+console.log("\nRésultat pour la division: ");
+console.log(6/3);
