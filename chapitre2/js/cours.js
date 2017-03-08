6ef69057335a2746a11ea1602683492018311932
Philippe Basuyau <geronimo8080@gmail.com>
2017-03-08 08:48:00 +0100

    ajout de la variable b, avec incrémentation

---
+++ chapitre2/js/cours.js
@@ -0,0 +1,8 @@
+var a;              //déclartion de la variable a
+a = 3.14;           //affectation de 3.14 à la variable a 
+console.log(a);     //Affiche la valeur de a
+
+var b=0;            // déclaration de la variable b
+b += 1;             // b contient  la valeur 1
+b++;                //b contient la valeur 2
+console.log(b);     //affiche la valeur de b
