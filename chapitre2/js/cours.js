ae9a3f9d66ef1b23de5ce132deaff76d56b63206
Philippe Basuyau <geronimo8080@gmail.com>
2017-03-08 09:04:06 +0100

    Travail sur l'expression

--- chapitre2/js/cours.js
+++ chapitre2/js/cours.js
@@ -6,3 +6,18 @@ var b=0;            // déclaration de la variable b
 b += 1;             // b contient  la valeur 1
 b++;                //b contient la valeur 2
 console.log(b);     //affiche la valeur de b
+
+// 3 est une expression dont la valeur est le nombre 3
+var c = 3;
+// c est une expression dont la valeur est celle de c (ici 3)
+var d = c;
+// (d + 1) est une expression
+// Sa valeur est celle de d augmentée de 1 (ici 4)
+d = d + 1; // d contient la valeur 4
+console.log(d); // Affiche 4
+
+
+var e = 3 + 2 * 4; // e contient la valeur 11
+console.log(e);
+e = (3 + 2) * 4; // e contient la valeur 20
+console.log(e);
