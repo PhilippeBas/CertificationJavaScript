13fba80c532f406ae664a0d989eb73b22d3d1317
Philippe Basuyau <geronimo8080@gmail.com>
2017-03-08 09:16:59 +0100

    Travail sur les convertions de types

--- chapitre2/js/cours.js
+++ chapitre2/js/cours.js
@@ -7,6 +7,10 @@ b += 1;             // b contient  la valeur 1
 b++;                //b contient la valeur 2
 console.log(b);     //affiche la valeur de b
 
+
+/*
+Travail sur les expressions
+*/
 // 3 est une expression dont la valeur est le nombre 3
 var c = 3;
 // c est une expression dont la valeur est celle de c (ici 3)
@@ -21,3 +25,17 @@ var e = 3 + 2 * 4; // e contient la valeur 11
 console.log(e);
 e = (3 + 2) * 4; // e contient la valeur 20
 console.log(e);
+
+/*
+Travail sur les conversions de type
+*/
+var f = 100; //déclaration de f et affectation à f de la valeur 100
+console.log("La valeur de f contient la valeur " + f);
+
+var g ="cinq" *2;
+console.log(g);//affiche NaN (None a Number)
+
+var h = "5";
+console.log(h + 1); // Concaténation : affiche la chaîne "51"
+h = Number("5");
+console.log(h + 1); // Addition numérique : affiche le nombre 6
