a26e1d2ff5f97efc16caa45a008bf7741c4a9cb6
Philippe Basuyau <geronimo8080@gmail.com>
2017-03-08 09:59:51 +0100

    If imbrique et if complet

--- chapitre3/js/cours.js
+++ chapitre3/js/cours.js
@@ -4,7 +4,34 @@ Les conditions
 
 
 // L'instrustion if
+
 var nombre = Number(prompt("Entrez un nombre : "));
-if (nombre > 0){
-    console.log(nombre +v" est positif");
+if (nombre >= 0) {
+    console.log(nombre + " est positif ou nul");
+}
+
+
+// avec un if imbriqué
+
+var nombre = Number(prompt("Entrez un nombre :"));
+if (nombre > 0) {
+    console.log(nombre + " est positif");
+} else { // nombre <= 0
+    if (nombre < 0) {
+        console.log(nombre + " est négatif");
+    } else { // nombre === 0
+        console.log(nombre + " est nul");
+    }
+}
+
+
+// avec un if, esle if et esle
+
+var nombre = Number(prompt("Entrez un nombre :"));
+if (nombre > 0) {
+    console.log(nombre + " est positif");
+} else if (nombre < 0) {
+    console.log(nombre + " est négatif");
+} else {
+    console.log(nombre + " est nul");
 }
