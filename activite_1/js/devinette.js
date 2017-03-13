4a02b55850fe6aab1f275c9963cf864c08c9d5f9
Philippe Basuyau <geronimo8080@gmail.com>
2017-03-13 15:40:55 +0100

    Modication de devinette.js pour pouvoir choisir soit même le nombre d'eassais

--- activite_1/js/devinette.js
+++ activite_1/js/devinette.js
@@ -23,27 +23,31 @@ console.log("(La solution est " + solution + ")");
 var nbrProposition = 0; // ici ma variable pour savoir compter le nbre de proposition
 var nombreChoisi = 0; // Ma variable pour le nombre choisi
 var compteurProposition = 6; // ma variable pour du compteur de Proposition 
-                             //--- on peut comme cela modifier le nbre d'essais à volonté
                             
+// --------------   Améloration choix du nombre d'essais possible ------------
+// Décommentez la ligne si dessous si vous voulez choisir un nbr d'essais 
+// var compteurProposition = Number(prompt("Choisi un nombre d'essai(s) : ")); 
+//----------------------------------------------------------------------------
+
 for (nbrProposition = 1; nbrProposition <= compteurProposition; nbrProposition++){
     var nombreChoisi = Number(prompt("Choisi un nombre compris entrer 1 et 100 : "));
     
-    if(nombreChoisi === solution){// si le chiffre est ok 
-        console.log("Bravo !  la solution était : " + solution);//j'indique qu'il a gagné et marque la solution
-        console.log("tu as trouvé en " + nbrProposition + " essai(s) ");
-            break;
+        if(nombreChoisi === solution){// si le chiffre est ok 
+            console.log("Bravo !  la solution était : " + solution);//j'indique qu'il a gagné et marque la solution
+                console.log("tu as trouvé en " + nbrProposition + " essai(s) ");
+                    break;
     }
     if((nombreChoisi < solution) && (nbrProposition !== compteurProposition)){
         console.log(nombreChoisi + " est trop petit ! "); // c'est +
     }
     if ((nombreChoisi > solution ) && (nbrProposition !== compteurProposition)){
-        console.log(nombreChoisi + "est trop grand ! ");  // c'est -
+            console.log(nombreChoisi + "est trop grand ! ");  // c'est -
     }
      else if((nombreChoisi < solution) && (nbrProposition === compteurProposition)){
-        console.log("Perdu ! La solution était: " + solution); // c'est + ... tu n'as plus essai
+            console.log("Perdu ! ... La solution était: " + solution); // c'est + ... tu n'as plus essai
     }
      else if ((nombreChoisi > solution ) && (nbrProposition === compteurProposition)){
-        console.log( "Perdu ! La solution était: " + solution);  // c'est -  ... tu n'as plus essai
+            console.log( "Perdu ! ... La solution était: " + solution);  // c'est -  ... tu n'as plus essai
     }
 }// fin de for
 
