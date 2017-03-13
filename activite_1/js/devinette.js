456bb481e6ce33731d622760a710ad4c6f6ba1c6
Philippe Basuyau <geronimo8080@gmail.com>
2017-03-13 15:28:33 +0100

    Jeux de devinette entre 1 et 100 en javaScript

---
+++ activite_1/js/devinette.js
@@ -0,0 +1,49 @@
+/* 
+Activité : jeu de devinette
+*/
+
+// NE PAS MODIFIER OU SUPPRIMER LES LIGNES CI-DESSOUS
+// COMPLETEZ LE PROGRAMME UNIQUEMENT APRES LE TODO
+
+console.log("Bienvenue dans ce jeu de devinette !");
+
+// Cette ligne génère aléatoirement un nombre entre 1 et 100
+var solution = Math.floor(Math.random() * 100) + 1;
+
+// Décommentez temporairement cette ligne pour mieux vérifier le programme
+console.log("(La solution est " + solution + ")");
+
+// TODO : complétez le programme
+
+/*
+---***--- Le jeu des Devinettes en JS ---***---
+*/
+
+//les Variables
+var nbrProposition = 0; // ici ma variable pour savoir compter le nbre de proposition
+var nombreChoisi = 0; // Ma variable pour le nombre choisi
+var compteurProposition = 6; // ma variable pour du compteur de Proposition 
+                             //--- on peut comme cela modifier le nbre d'essais à volonté
+                            
+for (nbrProposition = 1; nbrProposition <= compteurProposition; nbrProposition++){
+    var nombreChoisi = Number(prompt("Choisi un nombre compris entrer 1 et 100 : "));
+    
+    if(nombreChoisi === solution){// si le chiffre est ok 
+        console.log("Bravo !  la solution était : " + solution);//j'indique qu'il a gagné et marque la solution
+        console.log("tu as trouvé en " + nbrProposition + " essai(s) ");
+            break;
+    }
+    if((nombreChoisi < solution) && (nbrProposition !== compteurProposition)){
+        console.log(nombreChoisi + " est trop petit ! "); // c'est +
+    }
+    if ((nombreChoisi > solution ) && (nbrProposition !== compteurProposition)){
+        console.log(nombreChoisi + "est trop grand ! ");  // c'est -
+    }
+     else if((nombreChoisi < solution) && (nbrProposition === compteurProposition)){
+        console.log("Perdu ! La solution était: " + solution); // c'est + ... tu n'as plus essai
+    }
+     else if ((nombreChoisi > solution ) && (nbrProposition === compteurProposition)){
+        console.log( "Perdu ! La solution était: " + solution);  // c'est -  ... tu n'as plus essai
+    }
+}// fin de for
+
