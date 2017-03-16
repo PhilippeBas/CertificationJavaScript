d98f198b5d4c95e28a78622bff53fcf96e664ccc
Philippe Basuyau <geronimo8080@gmail.com>
2017-03-16 07:58:24 +0100

    Deplcement du fichier poo.html est chapitre 7

--- chapitre5/js/exo.js
+++ chapitre5/js/exo.js
@@ -83,3 +83,6 @@ console.log(calculer(4, "+", 6)); // Doit afficher 10
 console.log(calculer(4, "-", 6)); // Doit afficher -2
 console.log(calculer(2, "*", 0)); // Doit afficher 0
 console.log(calculer(12, "/", 0)); // Doit afficher Infinity
+
+
+
---
+++ chapitre7/js/poo.js
@@ -0,0 +1,22 @@
+/*
+Création objet
+*/
+var stylo = {
+    type: "bille",
+    couleur: "bleu",
+    marque: "bic"
+};
+console.log(stylo.type);
+
+/*
+Autre méthode 
+Technique très proche en Java, C# ou PHP
+*/
+
+//// Constructeur MonObjet
+//function MonObjet(){
+//    //initialisatopn de l'objet
+//    //...
+//}
+//// Instanciation d'un objet à partit du constructuer
+//var monObj = new MonObjet();
