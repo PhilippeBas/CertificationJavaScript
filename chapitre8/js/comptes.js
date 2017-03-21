b5ba229c47d11255aa6d27d458bbd7ab621ce5b1
Philippe Basuyau <geronimo8080@gmail.com>
2017-03-21 17:26:08 +0100

    Exo sur Compte

---
+++ chapitre8/js/comptes.js
@@ -0,0 +1,27 @@
+// TODO : ajoutez ici la définition des objets nécessaires
+
+
+
+
+//-----------------------------------------------------------------------
+
+
+var compte1 = Object.create(CompteBancaire);
+compte1.initCB("Alex", 100);
+var compte2 = Object.create(CompteEpargne);
+compte2.initCE("Marco", 50, 0.05);
+
+console.log("Voici l'état initial des comptes :");
+console.log(compte1.decrire());
+console.log(compte2.decrire());
+
+var montant = Number(prompt("Entrez le montant à transférer entre les comptes :"));
+compte1.debiter(montant);
+compte2.crediter(montant);
+
+// Calcule et ajoute les intérêts au solde du compte
+compte2.ajouterInterets();
+
+console.log("Voici l'état final des comptes après transfert et calcul des intérêts :");
+console.log(compte1.decrire());
+console.log(compte2.decrire());
