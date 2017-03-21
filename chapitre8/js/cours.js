12e371aef42c9c3f4a12f486d01a94ff195bc18e
Philippe Basuyau <geronimo8080@gmail.com>
2017-03-21 10:54:14 +0100

    chapitre8/js/cours.js

---
+++ chapitre8/js/cours.js
@@ -0,0 +1,12 @@
+/*
+Objets et prototypes en JavaScript
+*/
+
+var unObjet = {
+    a: 2
+};
+
+// Crée unAutreObjet avec unObjet comme prototype
+var unAutreObjet = Object.create(unObjet);
+
+console.log(unAutreObjet.a); // Affiche 2
