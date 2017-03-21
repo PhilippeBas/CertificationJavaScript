3e156670e924087173fe6c7b1c87dce0f76cbb88
Philippe Basuyau <geronimo8080@gmail.com>
2017-03-21 11:19:51 +0100

    chapitre8/js/cours.js fin du cours

--- chapitre8/js/cours.js
+++ chapitre8/js/cours.js
@@ -10,3 +10,9 @@ var unObjet = {
 var unAutreObjet = Object.create(unObjet);
 
 console.log(unAutreObjet.a); // Affiche 2
+
+// Créer encoreUnObjet avec unAutreObjet comme prototype
+var encoreUnObjet = Object.create(unAutreObjet);
+
+console.log(encoreUnObjet.a);// affiche 2
+console.log(encoreUnObjet.b); 
