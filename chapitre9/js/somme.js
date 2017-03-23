f531d936e614a98aa15586da91e0d8fba40126e2
Philippe Basuyau <geronimo8080@gmail.com>
2017-03-23 09:50:52 +0100

    exercice : additionner les valeurs du tableau, trouver la valeur max et la valeur mini

---
+++ chapitre9/js/somme.js
@@ -0,0 +1,35 @@
+/*
+Sommes
+*/
+
+//le tableau
+var valeurs = [11, 3, 7, 2, 9, 10];
+var somme = 0;
+var valeurMax = valeurs[0];
+var valeurMini = valeurs[0];
+
+for (var i = 0; i < valeurs.length; i++){
+    somme += valeurs[i];
+   // console.log(somme);
+}
+console.log("La somme des éléments vaut " + somme);
+
+// Valeur max
+
+for (var i = 0; i < valeurs.length; i++){
+    if (valeurs[i] > valeurMax){
+        valeurMax = valeurs[i];
+    }        
+}
+
+console.log("La valeur maximun du tableau est " + valeurMax);
+
+
+// Valeur mini
+for (var i = 0; i < valeurs.length; i++){
+    if (valeurs[i] < valeurMini){
+        valeurMini = valeurs[i];
+    }
+}
+console.log("La valeur minimun du tableau est ") + valeurMini;
+
